46c7d0f0888f24fea450df14862392f09fc316b7
DonkersKevin <donkers.kevin86@gmail.com>
2022-10-20 16:37:47 +0200

    Initial commit

---
+++ src/test/java/com/bizzybees/bizzybooky/BizzyBookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizzybees.bizzybooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizzyBookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
