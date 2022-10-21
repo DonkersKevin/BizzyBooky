26e0846791f9124bf88e43744a8d7aa9183d3d04
Harold van Doorslaer <haroldvandoorslaer@hotmail.com>
2022-10-21 15:03:22 +0200

    Story 3: commit with failing integration testing

--- src/test/java/com/bizzybees/bizzybooky/controllers/BookControllerIntegrationTest.java
+++ src/test/java/com/bizzybees/bizzybooky/controllers/BookControllerIntegrationTest.java
@@ -1,9 +1,9 @@
 package com.bizzybees.bizzybooky.controllers;
 
-import com.bizzybees.bizzybooky.domain.Book;
 import com.bizzybees.bizzybooky.domain.dto.BookDto;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,30 +12,35 @@ import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookControllerIntegrationTest {
-
     @LocalServerPort
     private int port;
 
     @Autowired
     BookController bookController;
+    List<BookDto> expectedBookList;
 
 
-
-    @Test
-    void getAllBooks() {
-        //ARRANGE
-        List<BookDto> expectedBookList = new ArrayList<>(List.of(
+    @BeforeEach
+    void init() {
+        expectedBookList = new ArrayList<>(List.of(
                 new BookDto("1000-2000-3000", "Pirates", "Mister", "Crabs", "Lorem Ipsum"),
                 new BookDto("2000-3000-4000", "Farmers", "Misses", "Potato", "Lorem Ipsum"),
                 new BookDto("3000-4000-5000", "Gardeners", "Miss", "Lettuce", "Lorem Ipsum"),
                 new BookDto("6000-7000-8000", "Programmes", "Boy", "Name", "Lorem Ipsum")
         ));
+    }
+
+
+    @Test
+    void WhenCallingBooks_GetFullBookListBack() {
+        //ARRANGE
 
         //ACT
         BookDto[] result = RestAssured
@@ -102,42 +107,21 @@ class BookControllerIntegrationTest {
 
     @Test
     void GivenStringIdOutOfBounds_ThrowNoSuchElement() {
-                    BookDto[] result = RestAssured
-                            //ARRANGE
-                            .given()
-                            .baseUri("http://localhost:8080")
-                            .port(port)
-                            //ACT
-                            .when()
-                            .accept(ContentType.JSON)
-                            .get("/books/8")
-                            .then()
-                            .assertThat()
-                            .extract()
-                            .as(BookDto[].class);
-
-        //ASSES
-        assertEquals(result, expectedBookList.get(2));
-    }
-
-    @Test
-    void getBookByIsbn_withWildCards(){
-        //Given
-        BookDto bookDto = new BookDto("1000-2000-3000", "Pirates", "Mister", "Crabs", "Lorem Ipsum");
-        //When
-        BookDto result = RestAssured
+        BookDto[] result = RestAssured
+                //ARRANGE
                 .given()
                 .baseUri("http://localhost:8080")
                 .port(port)
+                //ACT
                 .when()
                 .accept(ContentType.JSON)
-                .get("/*-2000-3000")
+                .get("/books/8")
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.OK.value())
                 .extract()
-                .as(BookDto.class);
-        //Then
-        assertThat(result).isEqualTo(bookDto);
+                .as(BookDto[].class);
+
+        //ASSES
+        assertEquals(result, expectedBookList.get(2));
     }
 }
