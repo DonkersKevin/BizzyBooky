41963b1ab91152e7c0a06f94d77220390866a440
DonkersKevin <donkers.kevin86@gmail.com>
2022-10-24 11:13:18 +0200

    minor changes, deactivated local logging

--- src/test/java/com/bizzybees/bizzybooky/controllers/BookControllerTest.java
+++ src/test/java/com/bizzybees/bizzybooky/controllers/BookControllerTest.java
@@ -34,18 +34,4 @@ class BookControllerTest {
         //Then
         Assertions.assertEquals(expectedBookList, bookRepositoryToTest);
     }
-
-
-
-    /**
-    List<BookDto> expectedBookList = new ArrayList<>(List.of(
-            new BookDto("1000-2000-3000", "Pirates", "Mister", "Crabs"),
-            new BookDto("2000-3000-4000", "Farmers", "Misses", "Potato"),
-            new BookDto("3000-4000-5000", "Gardeners", "Miss", "Lettuce"),
-            new BookDto("6000-7000-8000", "Programmes", "Boy", "Name")
-    ));
-     */
-
-
-
 }
