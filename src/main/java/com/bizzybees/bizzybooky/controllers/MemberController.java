38c3cc111f1ae5a510e8659983848165de022616
Harold van Doorslaer <haroldvandoorslaer@hotmail.com>
2022-10-26 10:32:54 +0200

    MemberController - Adding logger

--- src/main/java/com/bizzybees/bizzybooky/controllers/MemberController.java
+++ src/main/java/com/bizzybees/bizzybooky/controllers/MemberController.java
@@ -6,6 +6,8 @@ import com.bizzybees.bizzybooky.security.SecurityService;
 import com.bizzybees.bizzybooky.services.MemberService;
 import com.bizzybees.bizzybooky.domain.dto.memberdtos.NewMemberDto;
 import com.bizzybees.bizzybooky.domain.dto.memberdtos.ReturnMemberDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 @RequestMapping("/members")
 public class MemberController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     MemberService memberService;
     SecurityService securityService;
 
@@ -26,6 +30,7 @@ public class MemberController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ReturnMemberDto addMember(@RequestBody NewMemberDto newMemberDto) {
+        log.info("adding the following member: "+ newMemberDto);
         return memberService.addMember(newMemberDto);
     }
 
@@ -33,6 +38,7 @@ public class MemberController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/view", produces = MediaType.APPLICATION_JSON_VALUE)
     public ConcurrentHashMap<String, Member> getAllmembers(@RequestHeader String authorization) {
+        log.info("Retrieving the list of all registered members" );
         securityService.validateAuthorization(authorization, Feature.VIEW_MEMBERS);
         return memberService.getAllMembers();
         //ToDo Exclude INSS adn also return memberDto
